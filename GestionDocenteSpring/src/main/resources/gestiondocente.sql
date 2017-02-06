63e442d702ce73eabb549ec84c57872e39f127e2
Enrique J. Ruiz <enriquejruizj@gmail.com>
2017-02-06 12:03:05 +0100

    DB con estructura Actualizada

--- GestionDocenteSpring/src/main/resources/gestiondocente.sql
+++ GestionDocenteSpring/src/main/resources/gestiondocente.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-02-2017 a las 11:00:02
+-- Tiempo de generación: 06-02-2017 a las 12:01:09
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -17,11 +17,70 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `gestiongocente`
+-- Base de datos: `gestiondocente`
 --
-DROP DATABASE `gestiongocente`;
-CREATE DATABASE IF NOT EXISTS `gestiongocente` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `gestiongocente`;
+DROP DATABASE `gestiondocente`;
+CREATE DATABASE IF NOT EXISTS `gestiondocente` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `gestiondocente`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `fNacimineto` date DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poblacion` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `codigoPostal` int(5) DEFAULT NULL,
+  `telefono` int(9) NOT NULL,
+  `nHermanos` int(2) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`codigo`, `dni`, `nombre`, `apellidos`, `fNacimineto`, `email`, `direccion`, `poblacion`, `codigoPostal`, `telefono`, `nHermanos`, `activo`) VALUES
+(1, '45678912H', 'Enrique Javier', 'Ruiz Jiménez', '1985-12-01', 'enriquej@algomail.com', NULL, NULL, NULL, 678945123, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+CREATE TABLE IF NOT EXISTS `profesor` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `fNacimineto` date DEFAULT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poblacion` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `codigoPostal` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telefono` int(9) NOT NULL,
+  `nSS` bigint(12) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `profesor`
+--
+
+INSERT INTO `profesor` (`codigo`, `dni`, `nombre`, `apellidos`, `fNacimineto`, `email`, `direccion`, `poblacion`, `codigoPostal`, `telefono`, `nSS`) VALUES
+(1, '45612378R', 'Enrique Javier', 'Ruiz Jiménez', NULL, 'alguienporhay@klesden.com', NULL, NULL, NULL, 654987123, 123456789123);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
